a8813fe2b2a7a07dfb82cba2d91196d31da93f66
Valdes <211630@dadeschools.net>
2019-10-14 15:37:32 -0400

    getting token with demo values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core'
+import { NgForm, NgModel } from '@angular/forms'
 import { faCircle as farCircle } from '@fortawesome/free-regular-svg-icons'
 import { faInfoCircle, faCircle } from '@fortawesome/free-solid-svg-icons'
-// import { ngMOdel }
+import { TokenService } from './token.service'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,6 +13,8 @@ export class AppComponent {
   title = 'tokenClient'
   faInfoCircle = faInfoCircle
   faCircle = faCircle
+  selectedTab = 0
+  favorite = ''
   ContentType = 'x-www-form-urlencoded'
   grantType = 'password'
   apiUrl = 'http://api.dadeschools.net/token'
@@ -21,6 +25,21 @@ export class AppComponent {
   useRefreshTokenValue = ''
   useRefreshToken = false
   useClientAuthentication = false
+  postError = false
+  postErrorMessage = ''
+  tokenFullWrapper = {
+    access_token: '',
+    token_type: '',
+    expires_in: null,
+    token_issued: '',
+    token_expires: '',
+    claims: '',
+    settings: '',
+    apps: '',
+    refresh_token: ''
+  }
+  resourceData: ''
+  constructor(private tokenService: TokenService) {}
 
   useRefreshTokenChange() {
     if (this.useRefreshToken) {
@@ -29,8 +48,47 @@ export class AppComponent {
       this.grantType = 'password'
     }
   }
-
   grantTypeChanged() {
     this.useRefreshToken = this.grantType === 'refresh_token'
+  }
+  getTokenRequest() {
+    this.tokenService.GetToken().subscribe()
+  }
+  favoriteChanged() {
+    // populate all controls based on profile
+    console.log(this.favorite)
+  }
+  onSubmit(form: NgForm) {
+    console.log('in onSubmit:', form.valid)
+    if (form.valid) {
+      this.postError = false
+      this.tokenService.GetToken().subscribe(
+        result => {
+          console.log('success on component', result)
+          this.tokenFullWrapper = result
+          this.onTabChanged({index: 1})
+        },
+        error => {
+          this.onHttpError(error)
+        }
+      )
+    } else {
+      this.postError = true
+      this.postErrorMessage = 'Please fix the above errors.'
+    }
+  }
+  onHttpError(errorResponse: any) {
+    console.error('error: ' + errorResponse)
+    this.postError = true
+    this.postErrorMessage = errorResponse.error.errorMessage
+  }
+  onTabChanged(event) {
+    this.selectedTab = event.index
+  }
+  useTokenGetData() {
+    this.onTabChanged({ index: 2 })
+  }
+  useRefreshTokenGetToken() {
+    this.onTabChanged({ index: 0 })
   }
 }
--- src/app/token.service.ts
+++ src/app/token.service.ts
@@ -1,20 +1,41 @@
 import { Injectable } from '@angular/core'
-import { HttpClient, HttpErrorResponse } from '@angular/common/http'
-import { Observable } from 'rxjs'
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http'
+import { Observable, throwError } from 'rxjs'
 import { catchError, tap } from 'rxjs/operators'
 @Injectable({
   providedIn: 'root'
 })
 export class TokenService {
-  constructor(private http: HttpClient) { }
+  apiUrl = 'http://dev-api.dadeschools.net/token'
+  httpOptions = {
+    headers: new HttpHeaders({
+      Accept: 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    })
+  }
+  data = 'grant_type=password&username=211630&password=Logit@10&client_id=JS&client_secret=secret'
+  constructor(private http: HttpClient) {}
 
   GetToken(): Observable<any> {
-    return this.http.post<any>('url', data, httpOptions)
-      .pipe(catchError(this.handleError('Get token', data)))
-
+    return this.http.post<any>(this.apiUrl, this.data, this.httpOptions).pipe(
+      tap(
+        (data) => {
+        // console.log('All: ' + JSON.stringify(data))
+      }),
+      catchError(this.handleError)
+    )
   }
 
   private handleError(err: HttpErrorResponse) {
-    console.error(err.message + ' from handleError.')
+    let errorMessage = ''
+    if (err.error instanceof ErrorEvent) {
+      // client side error
+      errorMessage = `An error occurred: ${err.error.message}`
+    } else {
+      // server side error
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`
+    }
+    console.error(errorMessage) // email, log it, etc
+    return throwError(errorMessage)
   }
 }
