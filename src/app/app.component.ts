e21f574909a6a60059d12c450b5e5cf86976c44e
Valdes <211630@dadeschools.net>
2019-10-14 09:22:35 -0400

    UI fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component } from '@angular/core'
+import { faCircle as farCircle } from '@fortawesome/free-regular-svg-icons'
+import { faInfoCircle, faCircle } from '@fortawesome/free-solid-svg-icons'
+// import { ngMOdel }
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tokenClient';
+  title = 'tokenClient'
+  faInfoCircle = faInfoCircle
+  faCircle = faCircle
+  ContentType = 'x-www-form-urlencoded'
+  grantType = 'password'
+  apiUrl = 'http://api.dadeschools.net/token'
+  clientId = 'JS'
+  clientSecret = 'secret'
+  username = ''
+  passwordValue = ''
+  useRefreshTokenValue = ''
+  useRefreshToken = false
+  useClientAuthentication = false
+
+  useRefreshTokenChange() {
+    // this.useRefreshToken = !this.useRefreshToken
+    //alert(this.useRefreshToken)
+    if (this.useRefreshToken) {
+      this.grantType = 'refresh_token'
+    } else {
+      this.grantType = 'password'
+    }
+  }
+  /*  useClientAuthenticationChange() {
+    this.useClientAuthentication = !this.useClientAuthentication
+  } */
+  grantTypeChanged() {
+    this.useRefreshToken = this.grantType === 'refresh_token'
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './material.module'
+import { MatPasswordStrengthModule } from '@angular-material-extensions/password-strength'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule, BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NoopAnimationsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    MatPasswordStrengthModule.forRoot(),
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
