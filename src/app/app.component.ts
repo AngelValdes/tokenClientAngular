0b8de7e66ecd60265b9460fd097fa29971c07ea9
Angel Valdes <angelvaldes@hotmail.com>
2019-10-31 17:52:02 -0400

    added spinner to routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,16 @@ import { Component, OnInit } from '@angular/core'
 import { MatDialog } from '@angular/material/dialog'
 import { InformationDialogComponent } from './information-dialog/information-dialog.component'
 import { AuthService } from './common/auth.service'
-import { Router } from '@angular/router'
-// import { faCircle as farCircle } from '@fortawesome/free-regular-svg-icons'
+import {
+  Router,
+  Event,
+  NavigationStart,
+  NavigationEnd,
+  NavigationError,
+  NavigationCancel
+} from '@angular/router'
+import { faCircle as farCircle } from '@fortawesome/free-regular-svg-icons'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-root',
@@ -11,8 +19,11 @@ import { Router } from '@angular/router'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  faSpinner = faSpinner
   title = 'tokenClient'
-  mobile
+  mobile: boolean
+  loading = true
+  // pepe = true
   get isLoggedIn(): boolean {
     return this.authService.isLoggedIn
   }
@@ -22,7 +33,23 @@ export class AppComponent implements OnInit {
     }
     return ''
   }
-  constructor(public dialog: MatDialog, private authService: AuthService, private router: Router) {}
+  constructor(public dialog: MatDialog, private authService: AuthService, private router: Router) {
+    router.events.subscribe((routerEvent: Event) => {
+      this.checkRouterEvent(routerEvent)
+    })
+  }
+  checkRouterEvent(routerEvent: Event) {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true
+    }
+    if (
+      routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError
+    ) {
+      this.loading = false
+    }
+  }
   ngOnInit() {
     if (window.screen.width < 481) {
       this.mobile = true
